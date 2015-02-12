d2a131e98bb3701bbfd672a9217dc97f98067f8a
Walter Lau <walter@goodmorningstudio.com>
2015-02-11 21:49:57 -0800

    test app code

---
+++ cloud/app.js
@@ -0,0 +1,46 @@
+// These two lines are required to initialize Express in Cloud Code.
+var express = require('express');
+var app = express();
+
+// Global app configuration section
+app.set('views', 'cloud/views');  // Specify the folder to find templates
+app.set('view engine', 'ejs');    // Set the template engine
+app.use(express.bodyParser());    // Middleware for reading request body
+
+// This is an example of hooking up a request handler with a specific request
+// path and HTTP verb using the Express routing API.
+
+// use "app.get(...)" and "app.post(...)" for GET and POST
+
+app.get('/test', function(req, res) {
+  console.log('this is the homepage');
+  res.render('main', {});
+});
+
+app.get('/list', function(req, res) {
+  var query = new Parse.Query("_User");
+  var contacts = [];
+  query.descending("createdAt");
+  query.limit(5);
+  query.find().then(function(contacts) {
+    res.render('list_users', {users: contacts});
+  });
+});
+
+app.get('/add', function(req, res) {
+  var User = Parse.Object.extend("_User");
+  var user = new User();
+  user.set('username', 'user'+Math.ceil(Math.random()*100));
+  user.set('password', 'fission');
+  user.save(null, {
+    success: function(item) {
+      alert(item.get('username'));
+      res.render('new_user', {user: item});
+    },
+    error: function(item, error) {
+      alert(error.message);
+    }
+  });
+});
+
+app.listen();
--- cloud/main.js
+++ cloud/main.js
@@ -1,6 +1,11 @@
 
 // Use Parse.Cloud.define to define as many cloud functions as you want.
 // For example:
-Parse.Cloud.define("hello", function(request, response) {
+
+// Load our ExpressJS app
+
+require('cloud/app.js');
+
+Parse.Cloud.define("Hello", function(request, response) {
   response.success("Hello world!");
 });
